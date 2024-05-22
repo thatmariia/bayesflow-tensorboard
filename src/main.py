648bc1a8c7927f4ec3b1bb069e4590868c0b8bc0
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2024-05-22 22:42:45 +0200

    cleanup

--- src/main.py
+++ src/main.py
@@ -15,12 +15,6 @@ if __name__ == '__main__':
     os.makedirs(current_log_dir, exist_ok=True)  # Create the directory if it does not exist
     print("Logging to:", current_log_dir)
 
-    # check if current_log_dir is created
-    if os.path.exists(current_log_dir):
-        print("current_log_dir is created")
-    else:
-        print("current_log_dir is not created")
-
     run_minimal_example(current_log_dir)
 
 
